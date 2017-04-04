b9a2de0138089e895c6fd9c96c034d225c072689
Ashley MacWhirter <macwhirtera@gmail.com>
2017-04-04 00:11:07 -0400

    functional spotify & request

---
+++ liri.js
@@ -0,0 +1,174 @@
+//set up variables requiring npm packages
+
+var keys = require('./keys.js');
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+var inquirer = require('inquirer');
+var fs = require('fs');
+
+var client = new Twitter({
+    consumer_key: 'keys.twitterKeys.consumer_key',
+    consumer_secret: 'keys.twitterKeys.consumer_secret',
+    access_token_key: 'keys.twitterKeys.access_token_key',
+    access_token_secret: 'keys.twitterKeys.access_token_secret'
+});
+
+//Use inquirer for selection of commands
+
+inquirer.prompt([{
+        type: "list",
+        message: "What command would you like to run?",
+        choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
+        name: "commands"
+    }
+
+]).then(function(user) {
+    console.log(JSON.stringify(user, null, 2));
+
+    //Twitter========================================
+
+    if (user.commands === "my-tweets") {
+
+        var params = {
+            screen_name: "ashmac1088",
+            count: 20
+        };
+        client.get("statuses/user_timeline", params, function(error, tweets, response) {
+            if (!error) {
+                for (var i = 0; i < tweets.length; i++) {
+                    var user = params.screen_name;
+                    var tweets = tweets[i].text;
+                    var time = tweets[i].created_at;
+                    console.log("----------------" + time + "--------------------------");
+                    console.log("@" + user + " tweeted");
+                    console.log(tweets);
+                    console.log("------------------------------------------------------");
+                }
+                //console.log(tweets);
+            } else {
+                console.log("There was an err of " + error);
+            }
+        });
+
+        //Spotify===============================================
+
+    } else if (user.commands === "spotify-this-song") {
+
+        //prompt user to type in song to lookup - 'The Sign' by Ace of Base set as default
+
+        inquirer.prompt([{
+                type: "input",
+                message: "What song should I look up?",
+                name: "song",
+                default: "The Sign Ace of Base"
+            }
+
+            //set up Spotify search for Track based on "song" input by user
+
+        ]).then(function(response) {
+            spotify.search({
+                type: 'track',
+                query: response.song,
+                limit: 1
+            }, function(err, data) {
+                if (err) {
+                    console.log('Error occurred: ' + err);
+                    return;
+
+                    //set up base path for finding information through Spotify API
+
+                } else {
+                    var trackName = data.tracks.items;
+
+                    //for loop through results to pick out desired information
+
+                    for (var i = 0; i < 1; i++) {
+
+                        var trackData = trackName[i];
+
+                        var artists = trackData.artists[0].name;
+                        var song = trackData.name;
+                        var preview = trackData.preview_url;
+                        var album = trackData.album.name;
+
+                        //console log out the information found on the track provided by user
+
+                        console.log("============================================");
+                        console.log("");
+                        console.log("Artist: " + artists);
+                        console.log("Song Title: " + song);
+                        console.log("Album: " + album);
+                        console.log("Song Preview: " + preview);
+                        console.log("");
+                        console.log("============================================");
+                    }
+                }
+            });
+        });
+
+
+        //OMDB================================================
+
+    } else if (user.commands === "movie-this") {
+        inquirer.prompt([{
+                type: "input",
+                message: "What movie should I look up?",
+                name: "movie",
+                default: "Mr. Nobody"
+            }
+
+        ]).then(function(response) {
+
+                //run a request to the OMDB API with the movie specified by user
+
+                request("http://www.omdbapi.com/?t=" + response.movie + "&y=&plot=short&r=json", function(error, response, body) {
+
+                        // If there is no errir, and the request is successful (i.e. if the response status code is 200)
+
+                        if (!error && response.statusCode === 200) {
+
+                            // Parse the body of the site and recover the info needed
+
+                            console.log("======================================")
+                            console.log("Title: " + JSON.parse(body).Title);
+                            console.log("~~~~~~~~~~~~~~~~~~~~~")
+                            console.log("Release Date: " + JSON.parse(body).Year);
+                            console.log("~~~~~~~~~~~~~~~~~~~~~")
+                            console.log("IMBD rating is: " + JSON.parse(body).imdbRating);
+                            console.log("~~~~~~~~~~~~~~~~~~~~~")
+                            console.log("Produced in (country): " + JSON.parse(body).Country);
+                            console.log("~~~~~~~~~~~~~~~~~~~~~")
+                            console.log("Main language: " + JSON.parse(body).Language);
+                            console.log("~~~~~~~~~~~~~~~~~~~~~")
+                            console.log("Plot: " + JSON.parse(body).Plot);
+                            console.log("~~~~~~~~~~~~~~~~~~~~~")
+                            console.log("Actor's include: " + JSON.parse(body).Actors);
+                            console.log("======================================")
+                        }
+                    }
+                });
+        });
+});
+
+    //FS===========================================================
+
+}
+else if (user.commands === "do-what-it-says") {
+    // This block of code will read from the "random.txt" file.
+    // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+    // The code will store the contents of the reading inside the variable "data"
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        // We will then print the contents of data
+        console.log(data);
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+
+        // We will then re-display the content as an array for later use.
+        console.log(dataArr);
+
+    });
+}
+});
